0bd29c844df3348a093ab32c5346508a5f6836be
Ashutosh-NITH <ashutosh.93343.kumar@gmail.com>
2026-03-31 21:56:59 +0530

    without block

--- main.py
+++ main.py
@@ -88,9 +88,3 @@ async def websocket_chat_endpoint(websocket: WebSocket):
     finally:
         await websocket.close()
 
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 10000))
-    print(f"Starting server on port {port}")  # <-- helps debug
-    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info")
