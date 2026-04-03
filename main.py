54c55017002e90e50870652b26be319585f553a1
Ashutosh-NITH <ashutosh.93343.kumar@gmail.com>
2026-04-04 02:35:45 +0530

    main.py with bloc port 8000

--- main.py
+++ main.py
@@ -88,3 +88,6 @@ async def websocket_chat_endpoint(websocket: WebSocket):
     finally:
         await websocket.close()
 
+if __name__ == "__main__":
+    import uvicorn, os
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
